cbc3dc5f8a712b11e184593b24686fd93cc87384
Devina Ekawati <devina.ekawati@gmail.com>
2016-02-10 09:52:05 +0700

    Fix database connection

--- Room Management/src/booking/Booking.java
+++ Room Management/src/booking/Booking.java
@@ -17,62 +17,61 @@ import java.util.logging.Logger;
 
 public class Booking {
   
-  public Booking() {
+    private Database database;
+    private final String path = "jdbc:mysql://localhost:3306/room_management";
     
-  }
-  
-  public String addBooking(Borrowing borrowing) {
-    Database database = new Database();
-    database.connect("room-management");
-
-    String sql = "INSERT INTO borrowing (id_ruangan, id_peminjam, nama_peminjam, nomor_telepon_peminjam, "
-            + "status_peminjam, alamat_peminjam, nama_kegiatan, nama_lembaga, waktu_mulai, waktu_selesai, jumlah_peserta)"
-            + "VALUES (" + borrowing.getBorrowerId() + "," + borrowing.getBorrowerName() + "," + borrowing.getBorrowerPhone() + "," 
-            + borrowing.getBorrowerStatus() + "," + borrowing.getBorrowerAddress() + "," + borrowing.getActivityName() + "," + borrowing.getOrganizationName()
-            + borrowing.getStartTime() + "," + borrowing.getFinishTime() + "," + borrowing.getTotalParticipant() + ")";
-    return database.changeData(sql);
-  }
-  
-  public String changeBooking(Borrowing borrowing) {
-    Database database = new Database();
-    database.connect("room-management");
-
-    String sql = "UPDATE borrowing SET id_ruangan = " + borrowing.getId() + ", id_peminjam = " + borrowing.getBorrowerId()
-            + ", nama_peminjam = " + borrowing.getBorrowerName() + ", nomor_telepon_peminjam = " + borrowing.getBorrowerPhone() + "status_peminjam = "
-            + borrowing.getBorrowerStatus() + ", alamat_peminjam = " + borrowing.getBorrowerAddress() + ", nama_kegiatan = " + borrowing.getActivityName()
-            + ", nama_lembaga = " + borrowing.getOrganizationName() + ", waktu_mulai = " + borrowing.getStartTime() + ", waktu_selesai = " + borrowing.getFinishTime()
-            + ", jumlah_peserta = " + borrowing.getTotalParticipant() + " WHERE id_peminjaman = " + borrowing.getId();
-    return database.changeData(sql);
-  }
-  
-  public String deleteBooking(Borrowing borrowing) {
-    Database database = new Database();
-    database.connect("room-management");
-
-    String sql = "DELETE FROM peminjaman WHERE id_peminjaman = " + borrowing.getId();
-    return database.changeData(sql);
-  }
-  
-  public ArrayList<Borrowing> showClashBooking(Borrowing borrowing) {
-    ArrayList<Borrowing> clashBooking = new ArrayList<>();
-    Database database = new Database();
-    database.connect("room-management");
-    
-    String sql = "SELECT * FROM peminjaman WHERE id_ruangan = " + borrowing.getId() + " AND ((waktu_mulai >= " + borrowing.getStartTime() + "AND waktu_mulai <= "
-            + borrowing.getFinishTime() + ") OR (waktu_selesai >= " + borrowing.getStartTime() + " AND waktu_selesai <= " + borrowing.getFinishTime();
-    
-    ResultSet rs = database.fetchData(sql);
-    
-    try {
-      while (rs.next()) {
-        clashBooking.add(new Borrowing(rs.getInt("id_peminjaman"), rs.getInt("id_peminjam"), rs.getInt("id_ruangan"), rs.getString("nama_peminjam"),
-                rs.getString("status_peminjam"), rs.getString("alamat_peminjam"), rs.getString("nomor_telepon_peminjam"), rs.getString("nama_lembaga"), rs.getString("nama_kegiatan"), 
-                rs.getInt("jumlah_peserta"), rs.getTimestamp(""), rs.getTimestamp("waktu_mulai"), rs.getTimestamp("waktu_selesai")));
-      }
-      rs.close();
-    } catch (SQLException ex) {
-      Logger.getLogger(Booking.class.getName()).log(Level.SEVERE, null, ex);
+    public Booking() {
+        database = new Database();
     }
-    return clashBooking;
-  }
+
+    public String addBooking(Borrowing borrowing) {
+        database.connect(path);
+
+        String sql = "INSERT INTO borrowing (id_ruangan, id_peminjam, nama_peminjam, nomor_telepon_peminjam, "
+                + "status_peminjam, alamat_peminjam, nama_kegiatan, nama_lembaga, waktu_mulai, waktu_selesai, jumlah_peserta)"
+                + "VALUES (" + borrowing.getBorrowerId() + "," + borrowing.getBorrowerName() + "," + borrowing.getBorrowerPhone() + "," 
+                + borrowing.getBorrowerStatus() + "," + borrowing.getBorrowerAddress() + "," + borrowing.getActivityName() + "," + borrowing.getOrganizationName()
+                + borrowing.getStartTime() + "," + borrowing.getFinishTime() + "," + borrowing.getTotalParticipant() + ")";
+        return database.changeData(sql);
+    }
+
+    public String changeBooking(Borrowing borrowing) {
+        database.connect(path);
+
+        String sql = "UPDATE borrowing SET id_ruangan = " + borrowing.getId() + ", id_peminjam = " + borrowing.getBorrowerId()
+                + ", nama_peminjam = " + borrowing.getBorrowerName() + ", nomor_telepon_peminjam = " + borrowing.getBorrowerPhone() + "status_peminjam = "
+                + borrowing.getBorrowerStatus() + ", alamat_peminjam = " + borrowing.getBorrowerAddress() + ", nama_kegiatan = " + borrowing.getActivityName()
+                + ", nama_lembaga = " + borrowing.getOrganizationName() + ", waktu_mulai = " + borrowing.getStartTime() + ", waktu_selesai = " + borrowing.getFinishTime()
+                + ", jumlah_peserta = " + borrowing.getTotalParticipant() + " WHERE id_peminjaman = " + borrowing.getId();
+        return database.changeData(sql);
+    }
+
+    public String deleteBooking(Borrowing borrowing) {
+        database.connect(path);
+
+        String sql = "DELETE FROM peminjaman WHERE id_peminjaman = " + borrowing.getId();
+        return database.changeData(sql);
+    }
+
+    public ArrayList<Borrowing> showClashBooking(Borrowing borrowing) {
+        ArrayList<Borrowing> clashBooking = new ArrayList<>();
+        database.connect(path);
+
+        String sql = "SELECT * FROM peminjaman WHERE id_ruangan = " + borrowing.getId() + " AND ((waktu_mulai >= " + borrowing.getStartTime() + "AND waktu_mulai <= "
+                + borrowing.getFinishTime() + ") OR (waktu_selesai >= " + borrowing.getStartTime() + " AND waktu_selesai <= " + borrowing.getFinishTime();
+
+        ResultSet rs = database.fetchData(sql);
+
+        try {
+            while (rs.next()) {
+              clashBooking.add(new Borrowing(rs.getInt("id_peminjaman"), rs.getInt("id_peminjam"), rs.getInt("id_ruangan"), rs.getString("nama_peminjam"),
+                      rs.getString("status_peminjam"), rs.getString("alamat_peminjam"), rs.getString("nomor_telepon_peminjam"), rs.getString("nama_lembaga"), rs.getString("nama_kegiatan"), 
+                      rs.getInt("jumlah_peserta"), rs.getTimestamp(""), rs.getTimestamp("waktu_mulai"), rs.getTimestamp("waktu_selesai")));
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Booking.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return clashBooking;
+      }
 }
--- Room Management/src/maintenance/RoomMaintenance.java
+++ Room Management/src/maintenance/RoomMaintenance.java
@@ -18,50 +18,50 @@ import java.util.logging.Logger;
 
 public class RoomMaintenance {
   
-  public RoomMaintenance() {
+    private Database database;
+    private final String path = "jdbc:mysql://localhost:3306/room_management";
     
-  }
-  
-  public String addMaintenance(Maintenance maintenance) {
-    Database database = new Database();
-    database.connect("room-management");
-    
-    String sql = "INSERT INTO pemeliharaan (id_ruangan, deskripsi, waktu_mulai, waktu_selesai) VALUES (" +
-            maintenance.getRoomId() + " , " + maintenance.getDescription() + " , " + maintenance.getStartTime() +
-            " , " + maintenance.getFinishTime() + ")";
-    
-    return database.changeData(sql);
-  }
-  
-  public ArrayList<Maintenance> getClashMaintenance(Maintenance maintenance) {
-    ArrayList<Maintenance> clashMaintenance = new ArrayList<>();
-    
-    Database database = new Database();
-    database.connect("room-management");
-    
-    String sql = "SELECT * FROM pemeliharaan WHERE id_ruangan = " + maintenance.getRoomId() + " AND ((waktu_mulai >= " + 
-            maintenance.getStartTime() + " AND waktu_mulai <= " + maintenance.getFinishTime() + ") OR (waktu_selesai >= " +
-            maintenance.getStartTime() + " AND waktu_selesai <= " + maintenance.getFinishTime();
-    
-    ResultSet rs = database.fetchData(sql);
-    
-    try {
-      while(rs.next()) {
-        clashMaintenance.add(new Maintenance(rs.getInt("id_pemeliharaan"), rs.getInt("id_ruangan"), rs.getString("deskripsi"), rs.getTimestamp("waktu_mulai"), rs.getTimestamp("waktu_selesai")));
-      }
-    } catch (SQLException ex) {
-      Logger.getLogger(RoomMaintenance.class.getName()).log(Level.SEVERE, null, ex);
+    public RoomMaintenance() {
+        database = new Database();
     }
-    
-    return clashMaintenance;
-  }
-  
-  public String deleteBooking(Maintenance maintenance) {
-    Database database = new Database();
-    database.connect("room-management");
-    
-    String sql = "DELETE FROM peminjaman WHERE id_peminjaman = " + maintenance.getId();
-    
-    return database.changeData(sql);
-  }
+
+    public String addMaintenance(Maintenance maintenance) {
+        database.connect(path);
+
+        String sql = "INSERT INTO pemeliharaan (id_ruangan, deskripsi, waktu_mulai, waktu_selesai) VALUES (" +
+                maintenance.getRoomId() + " , " + maintenance.getDescription() + " , " + maintenance.getStartTime() +
+                " , " + maintenance.getFinishTime() + ")";
+
+        return database.changeData(sql);
+    }
+
+    public ArrayList<Maintenance> getClashMaintenance(Maintenance maintenance) {
+        ArrayList<Maintenance> clashMaintenance = new ArrayList<>();
+
+        database.connect(path);
+
+        String sql = "SELECT * FROM pemeliharaan WHERE id_ruangan = " + maintenance.getRoomId() + " AND ((waktu_mulai >= " + 
+                maintenance.getStartTime() + " AND waktu_mulai <= " + maintenance.getFinishTime() + ") OR (waktu_selesai >= " +
+                maintenance.getStartTime() + " AND waktu_selesai <= " + maintenance.getFinishTime();
+
+        ResultSet rs = database.fetchData(sql);
+
+        try {
+          while(rs.next()) {
+            clashMaintenance.add(new Maintenance(rs.getInt("id_pemeliharaan"), rs.getInt("id_ruangan"), rs.getString("deskripsi"), rs.getTimestamp("waktu_mulai"), rs.getTimestamp("waktu_selesai")));
+          }
+        } catch (SQLException ex) {
+          Logger.getLogger(RoomMaintenance.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return clashMaintenance;
+    }
+
+    public String deleteBooking(Maintenance maintenance) {
+        database.connect(path);
+
+        String sql = "DELETE FROM peminjaman WHERE id_peminjaman = " + maintenance.getId();
+
+        return database.changeData(sql);
+    }
 }
